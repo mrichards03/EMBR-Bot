804f8a798ad446dbe2ed654f23dbe43e37c21a5f
Mahatav Arora <96105216+Mahatav@users.noreply.github.com>
2024-04-28 20:04:01 -0700

    it can be accessed remotly now

--- IR_Camra/main.py
+++ IR_Camra/main.py
@@ -51,4 +51,4 @@ def video_feed():
     return Response(generate_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
 
 if __name__ == "__main__":
-    app.run(host='127.0.0.1', port=5000, debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
